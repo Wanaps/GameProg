2eb658953e8fc8aab759498204b9387c2921df1d
Mattéo Verdez <matteo.verdez@ynov.com>
2024-01-09 14:06:21 +0100

    Add asteroids

--- TP1/Assets/Scripts/Collisions.cs
+++ TP1/Assets/Scripts/Collisions.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collisions : MonoBehaviour
 {
+    private Text lostText;
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Collided.");
-        //delete the player
         Destroy(gameObject);
+        lostText = GameObject.Find("LostText").GetComponent<Text>();
+        lostText.text = "You Lost!";
     }
 }
