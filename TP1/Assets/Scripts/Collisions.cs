9a1cc09725c25e5bf7787f8feabb4e5fa01f059c
Ahliko <killian.guillemot@ynov.com>
2024-01-09 11:47:42 +0100

    change collide in edge camera

--- TP1/Assets/Scripts/Collisions.cs
+++ TP1/Assets/Scripts/Collisions.cs
@@ -4,10 +4,56 @@ using UnityEngine;
 
 public class Collisions : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D collision)
+    public float moveBackSpeed = 1.0f; // Vitesse de recul
+    private Rigidbody2D rb;
+    public float maxDistance = 0.001f; // Distance maximale entre le vaisseau et le bord de l'écran
+
+    void Start()
     {
-        Debug.Log("Collided.");
-        //delete the player
-        Destroy(gameObject);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Physics.Raycast(transform.position, transform.up, maxDistance))
+        {
+            // nothing in front, move forward
+            rb.velocity = -transform.up * moveBackSpeed;
+        } 
+        if (Physics.Raycast( transform.position, -transform.up, maxDistance))
+        {
+            // something behind, move forward
+            rb.velocity = transform.up * moveBackSpeed;
+        } 
+        if (Physics.Raycast( transform.position, transform.right, maxDistance))
+        {
+            // something on the right, move left
+            rb.velocity = -transform.right * moveBackSpeed;
+        } 
+        if (Physics.Raycast( transform.position, -transform.right, maxDistance))
+        {
+            // something on the left, move right
+            rb.velocity = transform.right * moveBackSpeed;
+        }
+        if (Physics.Raycast( transform.position, transform.up + transform.right, maxDistance))
+        {
+            // something on the right, move left
+            rb.velocity = -(transform.up + transform.right) * moveBackSpeed;
+        }
+        if (Physics.Raycast( transform.position, transform.up - transform.right, maxDistance))
+        {
+            // something on the left, move right
+            rb.velocity = -(transform.up - transform.right) * moveBackSpeed;
+        }
+        if (Physics.Raycast( transform.position, -transform.up + transform.right, maxDistance))
+        {
+            // something on the right, move left
+            rb.velocity = -(-transform.up + transform.right) * moveBackSpeed;
+        }
+        if (Physics.Raycast( transform.position, -transform.up - transform.right, maxDistance))
+        {
+            // something on the left, move right
+            rb.velocity = -(-transform.up - transform.right) * moveBackSpeed;
+        }
     }
 }
